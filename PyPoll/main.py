002767e1a317260e245584611b5d45fc501a9a03
mradovic10 <radovic.miodrag10@gmail.com>
2023-01-03 21:54:22 -0500

    The finishing touches for the challenge. Complete!

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,6 +5,7 @@ import csv
 # Locate the CSV file to be read.
 csvpath = os.path.join("Resources", "election_data.csv")
 
+# Create lists to hold votes for each individual candidate.
 charles = []
 diana = []
 raymon = []
@@ -16,59 +17,82 @@ with open(csvpath) as csvfile:
     # Skip the first row that contains the header.
     csvheader = next(csvfile)
 
+    # Loop through each row in the CSV file.
     for vote in csvreader:
 
+        # When there is a vote for Charles, add it to the "charles" list.
         if vote[2] == "Charles Casper Stockham":
             charles.append(vote[2])
-
+        
+        # When there is a vote for Diana, add it to the "diana" list.
         if vote[2] == "Diana DeGette":
             diana.append(vote[2])
 
+        # When there is a vote for Raymon, add it to the "raymon" list.
         if vote[2] == "Raymon Anthony Doane":
             raymon.append(vote[2])
 
+# Add up the lenghts of all three lists to get the total vote count.
 total = len(charles) + len(diana) + len(raymon)
 
+# Calculate the vote percentage for Charles and round the value to the third decimal point.
 charles_percentage = round((len(charles) / total) * 100, 3)
+# Calculate the number of votes for Charles.
 charles_count = len(charles)
 
+# Calculate the vote percentage for Diana and round the value to the third decimal point.
 diana_percentage = round((len(diana) / total) * 100, 3)
+# Calculate the number of votes for Diana.
 diana_count = len(diana)
 
+# Calculate the vote percentage for Raymon and round the value to the third decimal point.
 raymon_percentage = round((len(raymon) / total) * 100, 3)
+# Calculate the number of votes for Raymon.
 raymon_count = len(raymon)
 
+# Create a list that will hold the line to be written as to who the winner of the election is.
 winner_list = []
 
+# Create a function that will take the three candidate lists and figure out which one has the most votes.
 def winner(lists):
+    # If Charles has the most votes, then add his winner line to the "winner_list."
     if charles_count > diana_count and raymon_count:
         winner_list.append("Winner: Charles Casper Stockham")
 
+    # If Diana has the most votes, then add her winner line to the "winner_list."
     if diana_count > charles_count and raymon_count:
         winner_list.append("Winner: Diana DeGette")
 
+    # If Raymon has the most votes, then add his winner line to the "winner_list."
     if raymon_count > charles_count and diana_count:
         winner_list.append("Winner: Raymon Anthony Doane")
 
+# Activate the above function.
 winner([charles, diana, raymon])
 
+# Print out the required analysis to the terminal.
 print()
 print("Election Results")
 print()
 print("------------------------------")
 print()
+# Total number of votes in all.
 print(f"Total Votes: {total}")
 print()
 print("------------------------------")
 print()
+# Percent and count of the vote for Charles.
 print(f"Charles Casper Stockham: {charles_percentage}% ({charles_count})")
 print()
+# Percent and count of the vote for Diana.
 print(f"Diana DeGette: {diana_percentage}% ({diana_count})")
 print()
+# Percent and count of the vote for Raymon.
 print(f"Raymon Anthony Doane: {raymon_percentage}% ({raymon_count})")
 print()
 print("------------------------------")
 print()
+# Print out the first and only line of the "winner_list" to announce the winner.
 print(winner_list[0])
 print()
 print("------------------------------")
--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # python-challenge
 
-This is an update to the README file.
-
 https://github.com/mradovic10/python-challenge
+
+This is the Module 3 challenge for Python. It is completed according to the instructions provided on BootCampSpot.
+
+The challenge is split up into two folders: PyBank and PyPoll. Each folder is composed of an Analysis and Resources folder, in addition to the main python code file. The Analysis folder contains the text file with the written analysis, while the Resources folder contains the CSV file with the data used.
+
+I had used this resource to help me figure out how to successfully write into text files:
+https://www.pythontutorial.net/python-basics/python-write-text-file/
+
+I committed and pushed changes to github a couple of times, but in the future I should make more commits and pushes for the sake of safety.
+
+The python file for each challenge is hopefully satisfactorily commented.
+
+Happy 2023!
